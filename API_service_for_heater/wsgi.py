2e9e3efca02944a3cc8639d06feb439a3caa10e5
ira <irynaira@gmail.com>
2024-12-12 12:42:21 +0200

    First commit

---
+++ API_service_for_heater/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for API_service_for_heater project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'API_service_for_heater.settings')
+
+application = get_wsgi_application()
